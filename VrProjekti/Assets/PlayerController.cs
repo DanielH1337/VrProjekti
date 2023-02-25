403c3102704dcc0e9e1afe1a127eeafa6ed57950
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2023-02-25 13:51:51 +0200

    physics

--- VrProjekti/Assets/PlayerController.cs
+++ VrProjekti/Assets/PlayerController.cs
@@ -1,28 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 using Valve.VR.InteractionSystem;
+
+
 
 public class PlayerController : MonoBehaviour
 {
-    public bool showController = false;
+    public SteamVR_Action_Vector2 input;
+    public float speed=1;
+    private CharacterController characterContoller;
+
+    private void Start()
+    {
+        characterContoller = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(var hand in Player.instance.hands)
+        if(input.axis.magnitude > 0.1f)
         {
-            if (showController)
-            {
-                hand.ShowController();
-                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
-            }
-            else
-            {
-                hand.HideController();
-                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
-            }
+            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+            // transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction,Vector3.up);
+            characterContoller.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
         }
+       
+
         
+       
     }
 }
